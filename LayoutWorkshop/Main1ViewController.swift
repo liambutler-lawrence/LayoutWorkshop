966b298b11480e3a811d6a4fc300e6dbe4be2e44
davetek <dlawrence4994@gmail.com>
2019-09-15 10:50:31 -0400

    added image set and displayed image in custom view for tab bar button

--- LayoutWorkshop/Main1ViewController.swift
+++ LayoutWorkshop/Main1ViewController.swift
@@ -15,8 +15,23 @@ class Main1ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let navBarHeight: CGFloat = self.navigationController!.navigationBar.frame.height
+        print("navBarHeight: \(navBarHeight)")
+
+        let profileImage = UIImage(named: "tabBarButtonImage")
+        let profileImageView = UIImageView(image: profileImage)
+
+        //profileImageView.contentMode = .scaleAspectFit
+        profileImageView.widthAnchor.constraint(equalToConstant: navBarHeight)
+        profileImageView.heightAnchor.constraint(equalToConstant: navBarHeight)
+
+        if let leftBarButtonItem = self.navigationItem.leftBarButtonItem {
+            leftBarButtonItem.customView = profileImageView
+        }
     }
+    
+    
 
 
 }
