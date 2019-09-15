cc5d21483c90ecf6addb7f203a30408e2533cf3a
davetek <dlawrence4994@gmail.com>
2019-09-15 08:32:08 -0400

    Initial Commit

---
+++ LayoutWorkshop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutWorkshop
+//
+//  Created by David Lawrence on 9/15/19.
+//  Copyright © 2019 clarity for action. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
